e5a89d2188d84f8342f624968f0f3dbcb8f45780
Irtaza Sajid <stwaynexg@gmail.com>
2025-01-06 19:50:40 -0500

    added project_ids ghrb

--- scripts/run_ghrb.sh
+++ scripts/run_ghrb.sh
@@ -8,7 +8,7 @@ process_bug() {
     local bug=$2
 
     # Run the processing command
-    python3.9 postprocess_ghrb.py -p $proj -b $bug --exp_name "libro_default"
+    python3.9 postprocess_ghrb.py -p $proj -b $bug --exp_name "sc_baseline(libro)"
 }
 
 # Function to manage processing for a project
