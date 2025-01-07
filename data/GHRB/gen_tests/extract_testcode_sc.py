e0a21a538534b96496089ccc304bf1fb1a445ae2
Irtaza Sajid <stwaynexg@gmail.com>
2025-01-07 15:04:08 -0500

    extract code scripts added

---
+++ data/GHRB/gen_tests/extract_testcode_sc.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import os
+
+def main():
+    experiment = "meaningfulmask_0-rephrased"
+    df = pd.read_csv(f"{experiment}_generatedtestcases.csv")
+    df['Generated Testcases'] = df['Generated Testcases'].apply(eval)
+    os.makedirs("data/testcases_txt", exist_ok=True)
+    os.makedirs(f"data/testcases_txt/{experiment}", exist_ok=True)
+
+    for i, row in df.iterrows():
+        project = row['Project']
+        bug_number = row['Bug Number']
+        testcases = row['Generated Testcases']
+
+        for j, testcase in enumerate(testcases):
+            if not testcase.startswith("public void test"):
+                testcase = "public void test" + testcase
+            with open(f"{experiment}/{project}_{bug_number}_n{j}.txt", "w", errors='ignore') as f:
+                f.write(testcase)
+
+if __name__ == "__main__":
+    main()
